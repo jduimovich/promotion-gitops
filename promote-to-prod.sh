075ddd0f31c72c853c88d40fe36773585920682b
John Duimovich <jduimovich@gmail.com>
2024-03-09 09:45:23 -0500

    empty

--- promote-to-prod.sh
+++ promote-to-prod.sh
@@ -1,6 +1,4 @@
-BR=$(git rev-parse --abbrev-ref HEAD)
-git checkout -b  promote-stage-prod
-git checkout   promote-stage-prod
-git commit --allow-empty -m "Trigger promotion to stage $D" 
-git push --set-upstream origin  promote-stage-prod
-git checkout $BR
+echo "promote-stage-prod" > promotion-action
+git add promotion-action
+git commit -m "Promotion from Stage to Prod"
+git push
--- promote-to-stage.sh
+++ promote-to-stage.sh
@@ -1,8 +1,4 @@
-
-
-BR=$(git rev-parse --abbrev-ref HEAD)
-git checkout -b promote-dev-stage
-git checkout  promote-dev-stage
-git commit --allow-empty -m "Trigger promotion to stage $D" 
-git push --set-upstream origin promote-dev-stage
-git checkout $BR
+echo "promote-dev-stage" > promotion-action
+git add promotion-action
+git commit -m "Promotion from Dev to Stage"
+git push
